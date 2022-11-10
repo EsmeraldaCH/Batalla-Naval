a383aea654b7615aa3585f4401c73f39e1bcbc0c
EsmeraldaCH <115099254+EsmeraldaCH@users.noreply.github.com>
2022-11-10 16:13:47 -0600

    Add files via upload

---
+++ Batalla naval/actividad j.php
@@ -0,0 +1,7 @@
+/*
+<?php
+$resp1= $_GET["nombre"];
+$resp2= $_GET["apellido"];
+
+header("Location: battle.html");
+?>
---
+++ Batalla naval/re2jugadores.php
@@ -0,0 +1,9 @@
+<?php
+$resp1= $_GET["nombre1"];
+$resp2= $_GET["apellido1"];
+
+$resp3= $_GET["nombre2"];
+$resp4= $_GET["apellido2"];
+
+header("Location: Batalla .html");
+?>
